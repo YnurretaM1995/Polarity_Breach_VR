0a3b7bf8bd3238cc5f4e0810ab6172a5c20bb6d3
MarioHernandez <ynurretam@gmail.com>
2026-09-09 17:03:02 -0400

    Fixed tunneling in the bullet vs ennemies collision

--- Assets/Scripts/PolaritySystem/ShootProjectile.cs
+++ Assets/Scripts/PolaritySystem/ShootProjectile.cs
@@ -2,17 +2,16 @@ using PolarityBreach.Audio;
 using PolarityBreach.Boss;
 using PolarityBreach.Enemy;
 using PolarityBreach.Feedback;
+using PolarityBreach.PolaritySystem.Interfaces;
 using UnityEngine;
 
 namespace PolarityBreach.PolaritySystem
 {
-    
     [RequireComponent(typeof(PolarityComponent))]
     public class ShootProjectile : MonoBehaviour
     {
         [SerializeField] private TrailRenderer trail;
         [SerializeField] private ParticleSystem particles;
-
 
         [SerializeField] private float _lifeTime = 3f;
         [SerializeField] private bool _disapearOnHit = true;
@@ -21,19 +20,25 @@ namespace PolarityBreach.PolaritySystem
         [SerializeField] private AudioClip _impactSound;
         [SerializeField, Range(0f, 1f)] private float _impactSoundVolume = 1f;
         [SerializeField] private bool _playImpactSoundAs2D;
-        
+
+        [Header("Hit Detection")]
+        [SerializeField] private float hitRadius = 0.1f;
+
         private float _speed;
         private float _damage;
         private float _knockbackForce;
-        
+
         private PolarityComponent _polarity;
         private float _spawnTime;
 
         private void Awake() => _polarity = GetComponent<PolarityComponent>();
+
         private void OnEnable()
         {
             _spawnTime = Time.time;
+
             if (trail != null) trail.Clear();
+
             if (particles != null)
             {
                 particles.Clear();
@@ -43,11 +48,26 @@ namespace PolarityBreach.PolaritySystem
 
         private void Update()
         {
-            transform.position += transform.forward * (_speed * Time.deltaTime);
+            float step = _speed * Time.deltaTime;
+            Vector3 origin = transform.position;
+
+            transform.position += transform.forward * step;
+
+            RaycastHit[] hits = Physics.SphereCastAll(origin, hitRadius, transform.forward, step, ~0, QueryTriggerInteraction.Collide);
+
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (hits[i].collider == null) continue;
+                if (hits[i].collider.transform.IsChildOf(transform)) continue;
+                if (hits[i].collider.GetComponentInParent<IDamageable>() == null) continue;
+
+                HandleHit(hits[i].collider);
+                return;
+            }
+
             if (Time.time - _spawnTime >= _lifeTime) gameObject.SetActive(false);
         }
 
-        
         public void SetStats(float speed, float damage, float knockbackForce)
         {
             _speed = speed;
@@ -57,6 +77,13 @@ namespace PolarityBreach.PolaritySystem
 
         private void OnTriggerEnter(Collider other)
         {
+            HandleHit(other);
+        }
+
+        private void HandleHit(Collider other)
+        {
+            if (!gameObject.activeSelf) return;
+
             BossShield bossShield = other.GetComponentInParent<BossShield>();
             if (bossShield != null && bossShield.IsInvulnerable)
             {
@@ -81,11 +108,12 @@ namespace PolarityBreach.PolaritySystem
 
             EnemyHealth enemyHealth = other.GetComponentInParent<EnemyHealth>();
             bool hit = DamageSystem.TryApplyDamage(_polarity, other.gameObject, _damage);
+
             if (hit)
             {
                 Rigidbody rb = other.GetComponent<Rigidbody>();
                 Vector3 impactDirection = transform.forward;
-                FeedbackHandler.SpawnParticles(_impactEffect, transform.position,impactDirection);
+                FeedbackHandler.SpawnParticles(_impactEffect, transform.position, impactDirection);
 
                 if (enemyHealth == null || !enemyHealth.IsDead)
                 {
@@ -97,10 +125,10 @@ namespace PolarityBreach.PolaritySystem
                     Vector3 knockbackDirection = transform.forward;
                     knockbackDirection.y = 0;
                     knockbackDirection.Normalize();
-                    
+
                     rb.AddForce(knockbackDirection * _knockbackForce, ForceMode.Impulse);
                 }
-                
+
                 other.GetComponentInParent<PolarityVisual>()?.FlashHit();
 
                 if (_disapearOnHit)
